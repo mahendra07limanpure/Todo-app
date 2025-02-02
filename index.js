7537d639807254fc1d6e00ec157b74af24fae745
mahendra07limanpure <shashanklimanpure07@gmail.com>
2025-02-02 12:24:51 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.post("/todo",function(req,res){
+
+});
+
+app.get("/todos",function(re,req){
+
+});
+
+app.put("/completed",function(req,res){
+
+});
